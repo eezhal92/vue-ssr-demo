9b477450c7a4c1e3c7b23365a0906efe05a97d26
eezhal92 <arkinthesky.69@gmail.com>
2016-08-16 10:11:11 +0700

    flag one of fake note data as done

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,7 @@ export const fetchNotes = () => {
   let notes = [
     { id: chance.hash({length: 12}), title: 'Grab some food', desc: 'Just call go-food', color: '#fff', done: false },
     { id: chance.hash({length: 12}), title: 'Zzz', desc: 'Sleep', color: '#fff', done: false },
-    { id: chance.hash({length: 12}), title: 'Meh', desc: 'Dunno wut to do', color: '#77DD77', done: false },
+    { id: chance.hash({length: 12}), title: 'Meh', desc: 'Dunno wut to do', color: '#77DD77', done: true },
   ]
 
   return Promise.resolve(notes)
